3e58965264dc54cf461f4bd3ae60d5380c91e519
Faruk <adekoladamilola4@gmail.com>
2024-03-13 18:04:12 +0100

    Done with task

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+def isWinner(x, nums):
+    def sieve_of_eratosthenes(n):
+        primes = [True] * (n + 1)
+        primes[0] = primes[1] = False
+        p = 2
+        while p * p <= n:
+            if primes[p]:
+                for i in range(p * p, n + 1, p):
+                    primes[i] = False
+            p += 1
+        return [i for i in range(n + 1) if primes[i]]
+
+    def calculate_winner(n):
+        primes = sieve_of_eratosthenes(n)
+        num_primes = len(primes)
+        if num_primes % 2 == 0:
+            return "Ben"
+        else:
+            return "Maria"
+
+    winners = [calculate_winner(n) for n in nums]
+    ben_wins = winners.count("Ben")
+    maria_wins = winners.count("Maria")
+
+    if ben_wins == maria_wins:
+        return None
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return "Maria"
