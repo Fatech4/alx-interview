aa4fd362559fd7a1ff54c735d9d8bbcfc03d84c7
Faruk <adekoladamilola4@gmail.com>
2024-04-03 11:18:04 +0100

    fixes

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,31 +1,28 @@
 #!/usr/bin/python3
-""" Given a pile of coins of different values, determine the fewest number
-of coins needed to meet a given amount total """
+"""
+Task 0: Change comes from within
+Given a pile of coins of different values,
+determine the fewest number of coins needed
+to meet a given amount
+"""
 
 
 def makeChange(coins, total):
-    if total <= 0:
-        return 0
-    """ Initialize a list to store the minimum number of coins
-    needed for each total amount """
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0  # Base case: 0 coins needed to make a total of 0
-    # Iterate through all possible total amounts up to the given total
-    for i in range(1, total + 1):
-        """ For each total amount, iterate through all available coin
-        denominations """
-        for coin in coins:
-            """ If the current coin denomination is less than or equal
-            to the current total amount,
-            # check if using this coin plus the minimum number of
-            coins needed for the remaining amount
-            # results in a smaller number of coins than what we
-            have previously calculated. """
-            if coin <= i:
-                dp[i] = min(dp[i], dp[i - coin] + 1)
-    """ If the value of dp[total] remains float('inf'), it means
-    it's not possible to make the total amount with the given coins """
-    if dp[total] == float('inf'):
-        return -1
+    """
+    Returns the fewest number of coins needed to meet total
+    """
+    coinsUsed = 0
+    current = 0
 
-    return dp[total]
+    if total > 0:
+        coins = reversed(sorted(coins))
+
+        for coin in coins:
+            while current + coin <= total:
+                coinsUsed += 1
+                current += coin
+
+        if current != total:
+            return -1
+
+    return coinsUsed
